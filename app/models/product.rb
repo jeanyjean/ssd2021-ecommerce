d71760c3168bf061e08f075eeda9d84d0c286c98
jeanyjean <purich.double@gmail.com>
2021-03-25 20:30:53 +0700

    Add category model, page, and make it show on product page

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
     validates :stock, numericality: {greater_than_or_equal_to: 0}
     validates :title, length: { minimum: 1 }
     validates :description, length: { minimum: 1 }
+
+    has_many :categories_products
+    has_many :categories, through: :categories_products
 end
