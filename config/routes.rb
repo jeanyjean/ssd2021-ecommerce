adc27b01e31518bd3b8f25e7651ccf2ed03d8f9f
jeanyjean <purich.double@gmail.com>
2021-04-14 02:08:33 +0700

    Add primary image and supporting images and change path to admins/products

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :categories
   devise_for :admins
-  resources :products
   root 'home#index'
 
-  namespace :products do
-    post 'csv_upload'
+  namespace :admins do
+    resources :products
+    namespace :products do
+      post 'csv_upload'
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
