fca6427b4f470877ebb93532148f930f2bad2e9f
jeanyjean <purich.double@gmail.com>
2021-04-19 00:39:31 +0700

    Frontend on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @search = params[:search]
 
+    @products = Product.all
+    @products = @products
+      .search(@search) if @search.present?
+    @products = @products.page(params[:page]).per(8)
   end
 end
